6ca704d18e5d4b8c9f93fa0e7a0c08c91b9e2c7a
TanhaPatel <trpatel2906@gmail.com>
2019-10-12 15:43:46 +0530

    datepicker and timepicker

--- app/src/main/java/com/example/nav/AddTaskTxt.java
+++ app/src/main/java/com/example/nav/AddTaskTxt.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.internal.NavigationMenuItemView;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -34,14 +35,13 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Random;
 
 public class AddTaskTxt extends AppCompatActivity /*implements NavigationView.OnNavigationItemSelectedListener*/ {
 
-    DatePicker pickerDate;
-    TimePicker pickerTime;
     EditText addTaskEditText;
     Button addTaskBtn, txtdate, txttime;
 
@@ -50,11 +50,11 @@ public class AddTaskTxt extends AppCompatActivity /*implements NavigationView.On
     DatabaseReference databaseReference;
     Date datentime;
     String userid, email;
-    private Calendar calendarView;
     private FirebaseUser firebaseUser;
 
     private Context context = this;
     ImageView dateicon, timeicon;
+    TextView set_date, set_time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,43 +72,17 @@ public class AddTaskTxt extends AppCompatActivity /*implements NavigationView.On
         databaseReference = firebaseDatabase.getReference("Task");
         firebaseUser = firebaseAuth.getCurrentUser();
 
-        //pickerDate = findViewById(R.id.pickerdate);
-        //pickerTime = findViewById(R.id.pickertime);
-        /*Calendar now = Calendar.getInstance();
-        pickerDate.init(
-                now.get(Calendar.YEAR),
-                now.get(Calendar.MONTH),
-                now.get(Calendar.DAY_OF_MONTH),
-                null);
-        pickerTime.setCurrentHour(now.get(Calendar.HOUR_OF_DAY));
-        pickerTime.setCurrentMinute(now.get(Calendar.MINUTE));
-
-        addTaskBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Calendar cal = Calendar.getInstance();
-                cal.set(pickerDate.getYear(),
-                        pickerDate.getMonth(),
-                        pickerDate.getDayOfMonth(),
-                        pickerTime.getCurrentHour(),
-                        pickerTime.getCurrentMinute(),
-                        00);
-
-                datentime = cal.getTime();
-                addtask();
-            }
-        });*/
-
         txtdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final Dialog datedialog = new Dialog(context);
                 datedialog.setContentView(R.layout.custom_date_dialog);
-                ImageButton dateicon = datedialog.findViewById(R.id.dateicon);
+                dateicon = datedialog.findViewById(R.id.dateicon);
                 dateicon.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        Toast.makeText(AddTaskTxt.this, "DatePicker", Toast.LENGTH_SHORT).show();
+                        DialogFragment date = new DatePickerFragment();
+                        date.show(AddTaskTxt.this.getSupportFragmentManager(), "Date Picker");
                     }
                 });
                 datedialog.show();
@@ -120,24 +94,17 @@ public class AddTaskTxt extends AppCompatActivity /*implements NavigationView.On
             public void onClick(View v) {
                 final Dialog timedialog = new Dialog(context);
                 timedialog.setContentView(R.layout.custom_time_dialog);
-                ImageButton timeicon = timedialog.findViewById(R.id.timeicon);
+                timeicon = timedialog.findViewById(R.id.timeicon);
                 timeicon.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        Toast.makeText(AddTaskTxt.this, "TimePicker", Toast.LENGTH_SHORT).show();
+                        DialogFragment time = new TimePickerFragment();
+                        time.show(getSupportFragmentManager(), "Time Picker");
                     }
                 });
                 timedialog.show();
             }
         });
-
-        /*// toolbar activity starts
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitle("Task Planner");
-        toolbar.setSubtitle("Systematise your day!!!");
-
-        // toolbar activity ends*/
     }
 
     //data using username
@@ -166,55 +133,4 @@ public class AddTaskTxt extends AppCompatActivity /*implements NavigationView.On
 
         }
     }
-
-    /*@Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_addtask_txt) {
-            startActivity(new Intent(AddTaskTxt.this, AddTaskTxt.class));
-
-        } else if (id == R.id.nav_addtask_voice) {
-            startActivity(new Intent(AddTaskTxt.this, AddTaskVoice.class));
-
-        } else if (id == R.id.nav_agenda) {
-            startActivity(new Intent(AddTaskTxt.this, ViewTask.class));
-
-        } else if (id == R.id.nav_cng_pass) {
-            startActivity(new Intent(AddTaskTxt.this, ResetPassword.class));
-
-        } else if (id == R.id.nav_del_acc) {
-            firebaseUser.delete();
-            startActivity(new Intent(this, Login.class));
-
-        } else if (id == R.id.nav_logout) {
-            //logging out the user
-            firebaseAuth.signOut();
-            //closing activity
-            signOut();
-            finish();
-            //starting login activity
-            startActivity(new Intent(this, Login.class));
-
-        } else if (id == R.id.nav_info) {
-            startActivity(new Intent(this, Info.class));
-
-        } else if (id == R.id.nav_share) {
-            Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
-            sharingIntent.setType("text/plain");
-            String shareBodyText = "Check it out. Your message goes here";
-            sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject here");
-            sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBodyText);
-            startActivity(Intent.createChooser(sharingIntent, "Sharing Options"));
-            return true;
-        }
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-    private void signOut() {
-
-    }*/
 }
